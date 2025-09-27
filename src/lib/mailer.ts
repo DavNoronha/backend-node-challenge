f7f41e25e1c35ce11a22208f363883e9771f379c
Davnoronha <davidnd97@gmail.com>
2025-09-27 10:18:42 -0300

    fix:updating mailer config

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,11 +1,20 @@
 import nodemailer from "nodemailer"
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT) || 587,
-  secure: false, // true para 465, false para 587
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    ciphers: 'SSLv3',
+    rejectUnauthorized: false
+  },
+  connectionTimeout: 60000, // 60 segundos
+  greetingTimeout: 30000,   // 30 segundos
+  socketTimeout: 60000      // 60 segundos
 })
