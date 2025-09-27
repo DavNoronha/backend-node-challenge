ba23c1e7d46ca3e7a5476559c819d19fecb3b89c
Davnoronha <davidnd97@gmail.com>
2025-09-27 10:52:09 -0300

    feat:changing mailer package for prod

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,20 +1,6 @@
-import nodemailer from "nodemailer"
+import sgMail from '@sendgrid/mail'
 
-export const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-  tls: {
-    ciphers: 'SSLv3',
-    rejectUnauthorized: false
-  },
-  connectionTimeout: 60000, // 60 segundos
-  greetingTimeout: 30000,   // 30 segundos
-  socketTimeout: 60000      // 60 segundos
-})
+// Configure SendGrid
+sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
+
+export { sgMail }
